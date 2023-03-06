476b44686052b3ac219b2e698fcf71e0a0b50329
Starry-OvO <starry.qvq@gmail.com>
2023-03-06 21:18:14 +0800

    docs: fix comment

--- rotate_captcha_crack/dataset/typing.py
+++ rotate_captcha_crack/dataset/typing.py
@@ -7,7 +7,7 @@ class TypeImgTsSeq(Protocol):
     """
     Methods:
         - `def __len__(self) -> int:` length of the dataset
-        - `def __getitem__(self, idx: int) -> Tensor:` get img_tensor ([C,H,W]=[3,ud,ud], dtype=float32, range=[0.0,1.0))
+        - `def __getitem__(self, idx: int) -> Tensor:` get img_tensor ([C,H,W]=[3,ud,ud], dtype=uint8, range=[0,255])
     """
 
     def __len__(self) -> int:
