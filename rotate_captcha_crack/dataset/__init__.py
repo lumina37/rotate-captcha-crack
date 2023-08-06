3a872dbe41adb7498e99c07b46b2e84479347a8e
Starry-OvO <starry.qvq@gmail.com>
2023-08-06 11:20:16 +0800

    refactor: rename `dataset.val` to `validate`

--- rotate_captcha_crack/dataset/__init__.py
+++ rotate_captcha_crack/dataset/__init__.py
@@ -4,4 +4,4 @@ from .helper import rotate_square, square_resize, strip_border, to_square, to_te
 from .regress import RegDataset
 from .typing import TypeImgTsSeq
 from .utils import from_google_streetview
-from .val import ValDataset
+from .validate import ValDataset
